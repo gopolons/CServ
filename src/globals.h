6c503df3b1b91bb6f30e692841ca77b148b76e59
Georgii Polonskii <gopolons@gmail.com>
2024-05-25 11:09:38 +0100

    added a global header containing external config value for port

---
+++ src/globals.h
@@ -0,0 +1,11 @@
+#ifndef Globals_h
+#define Globals_h
+
+/*
+ * Variable used by receiver and argument handler
+ * determining the port on which the stream socket
+ * is going to be listening for incoming connections.
+ */
+extern char port[5];
+
+#endif // !Globals_h
